c4aae991380ce52f2856a6ab19200fca8c0dacd5
R4mii <40874154+R4mii@users.noreply.github.com>
2020-06-08 04:58:41 +0100

    Add files via upload

---
+++ termux-By-R4mii.py
@@ -0,0 +1,121 @@
+import os
+import sys
+
+print("------------------------------------------------------------------")
+print("------------------------------------------------------------------")
+print("------------------------------------------------------------------")
+print("------------------------------------------------------------------")
+print("""\033[1;91m\n                                                  
+
+
+8888888b.     d8888                d8b d8b      
+888   Y88b   d8P888                Y8P Y8P      
+888    888  d8P 888                             
+888   d88P d8P  888  88888b.d88b.  888 888      
+8888888P" d88   888  888 "888 "88b 888 888      
+888 T88b  8888888888 888  888  888 888 888      
+888  T88b       888  888  888  888 888 888      
+888   T88b      888  888  888  888 888 888      
+                                                
+                                                
+                                                
+
+  
+                                                  
+     \n\033[1;m""")
+
+print("------------------------------------------------------------------")
+print("----------------------Termux-tools-By-R4mii-----------------------")
+print("------------------------------------------------------------------")
+print("------------------------------------------------------------------")
+
+coco = os.system("echo $(tput setaf 1)Take a cup of coffee $(tput setab 7)Good luck$(tput sgr 0)")
+
+install = os.system("apt update")
+
+install1 = os.system("pkg install update")
+
+install2 = os.system("pkg install upgrade")
+
+install3 = os.system("apt-get update")
+
+install4 = os.system("apt-get update")
+
+install5 = os.system("apt-get upgrade")
+
+install6 = os.system("apt install php")
+
+install7 = os.system("apt install python")
+
+install8 = os.system("apt install python2")
+
+install9 = os.system("apt install git")
+
+install10 = os.system("apt install golang")
+
+install11 = os.system("apt install host")
+
+install = os.system("apt install nano")
+
+install = os.system("apt install havij")
+
+install = os.system("apt install hydra")
+
+install = os.system("apt install wireshark")
+
+install = os.system("apt install cmatrix")
+
+install = os.system("pkg install figlet")
+
+install = os.system("pkg install wget")
+
+install = os.system("pkg install wget -y")
+
+install = os.system("pkg install python2 -y")
+
+install = os.system("pkg install python2-dev -y")
+
+install = os.system("apt install wireshark")
+
+install = os.system("pkg install cowsay")
+
+install = os.system("pkg install toilet")
+
+install = os.system("pkg install ruby")
+
+install = os.system("pkg install help")
+
+install = os.system("gem install lolcat")
+
+install = os.system("pkg install curl")
+
+install = os.system("pkg install wgetrc")
+
+install = os.system("pkg install unzip")
+
+install = os.system("pkg install openssh")
+
+install = os.system("pkg install tor")
+
+install = os.system("pkg install uzip")
+
+install = os.system("pkg install net-tools")
+
+install = os.system("pkg install unrar")
+
+install = os.system("pkg install clang")
+
+install = os.system("pkg install w3m")
+
+install = os.system("pkg install proot")
+
+install = os.system("pip2 install wget")
+
+install = os.system("pip2 install requests")
+
+install = os.system("apt update && apt upgrade")
+
+
+echo = os.system("echo tbarklah 3lik")
+
+
